d2de874e2ea81a1d83ceaf7aaeb9faff86f83820
hammdata <gohammdata@gmail.com>
2020-06-19 14:54:18 -0400

    Jinja templating

--- src/init.py
+++ src/init.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your Browser is {}</p>'.format(user_agent)
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
