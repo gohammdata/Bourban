64b60be8e199ac04891b715ba95a23a4f687ab7f
hammdata <gohammdata@gmail.com>
2020-06-21 20:37:19 -0400

    Date Fromat and base

--- src/init.py
+++ src/init.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -18,5 +20,5 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
+moment = Moment(app)
 bootstrap = Bootstrap(app)
