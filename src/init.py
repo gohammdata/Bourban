0a82d46ed61497ca42094eec594d27eeba80e5fd
gohammdata <gohammdata@gmail.com>
2020-08-20 10:12:39 -0400

    Date Time Update for in browser.

--- src/init.py
+++ src/init.py
@@ -4,10 +4,12 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 app = Flask(__name__)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    return render_template('index.html',
+    current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
